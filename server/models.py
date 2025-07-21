c88fc5053099e7d13524997441e864b5c17298eb
JayceEse <jayce.ese@gmail.com>
2025-07-21 22:54:15 +1000

    create database models

---
+++ server/models.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, Boolean, Date, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "sqlite:///./tasks.db"
+
+Base = declarative_base()
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread: False"})
+SessionLocal = sessionmaker(autocommit=False, autoFlush=False, bind=engine)
+
+class Taskl(Base):
+    __tablename__= "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, nullable=True)
+    due_date = Column(Date, nullable=True)
+    completed = Column(Boolean, default=False)
+    tags = Column(String, nullable=True)
