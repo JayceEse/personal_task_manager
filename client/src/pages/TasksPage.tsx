1ff4a416a79a8ea4afeba248e0afa6a69542acf9
JayceEse <jayce.ese@gmail.com>
2025-08-05 21:38:25 +1000

    updated TaskPage.tsx

--- client/src/pages/TasksPage.tsx
+++ client/src/pages/TasksPage.tsx
@@ -1,9 +1,26 @@
-const TasksPage = () => {
+import { useState } from "react"
+import type { Task } from '../types/task.ts'
+
+export default function TasksPage() {
+    const [tasks, setTasks] = useState<Task[]>([
+        { id: 1, title: 'Buy groceries', completed: false},
+        { id: 2, title: 'Workout', completed: false},
+        { id: 3, title: 'Finish React project', completed: false}
+    ])
+
     return (
-        <div className="p-4">
-            <h1 className="text-2xl font-bold">Tasks Page</h1>
+        <div>
+            <h1 className="text-xl font-bold mb-4 ">My Tasks</h1>
+            <ul>
+                {tasks.map((task) => (
+                    <li 
+                    key={task.id} 
+                    className={`p-3 rounded shadow ${task.completed ? 'bg-green-100 line-through' : 'bg-white'}`}
+                    >
+                        {task.title}
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
-
-export default TasksPage
