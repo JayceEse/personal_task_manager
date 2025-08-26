956dcecdb76caaa4bbaca44574777f149707287e
JayceEse <jayce.ese@gmail.com>
2025-08-27 07:05:51 +1000

    added delete button

--- client/src/components/TaskList.tsx
+++ client/src/components/TaskList.tsx
@@ -3,9 +3,10 @@ import type { Task } from '../types/task';
 type TaskListProps = {
     tasks: Task[];
     onToggleTask: (id: number) => void;
+    onDeleteTask: (id: number) => void;
 };
 
-export default function TaskList ({tasks, onToggleTask}: TaskListProps) {
+export default function TaskList ({tasks, onToggleTask, onDeleteTask}: TaskListProps) {
     return (
         <ul>
             {tasks.map((task) => (
@@ -14,9 +15,19 @@ export default function TaskList ({tasks, onToggleTask}: TaskListProps) {
                 className={`p-3 rounded shadow cursor-pointer ${
                     task.completed ? 'bg-green-100 line-through' : 'bg-white'
                 }`}
-                onClick={() => onToggleTask(task.id)}
                 >
-                    {task.title}
+                    <div
+                    onClick={() => onToggleTask(task.id)}
+                    className='cursor-pointer flex-1/2'
+                    >
+                        {task.title}
+                    </div>
+                    <button
+                        onClick={() => onDeleteTask(task.id)}
+                        className='ml-4 bg-red-500 text-white px-2 py-1 rounded'
+                    >
+                        Delete
+                    </button>
                 </li>
             ))}
         </ul>
--- client/src/pages/TasksPage.tsx
+++ client/src/pages/TasksPage.tsx
@@ -8,6 +8,8 @@ export default function TasksPage() {
         {id: 2, title: 'Build Task Manager', completed: false}
     ])
 
+    const [newTask, SetNewTask] = useState('');
+
     // Will toggle task and put line through it when completed
     const toggleTask = (id: number) => {
         setTasks((prev) =>
@@ -17,10 +19,42 @@ export default function TasksPage() {
         );
     };
 
+    const addTask = () => {
+        if (!newTask.trim()) return;
+        const newTaskObj : Task = {
+            id: Date.now(),
+            title: newTask,
+            completed: false,
+        };
+        setTasks((prev) => [...prev, newTaskObj])
+        SetNewTask('');
+    }
+
+    const deleteTask = (id: number) => {
+        setTasks((prev) => prev.filter((task) => task.id !== id));
+    }
+
     return (
         <div className="p-4">
             <h1 className="text-xl font-bold mb-4">Tasks</h1>
-            <TaskList tasks={tasks} onToggleTask={toggleTask}/>
+            {/* New Task Input */}
+            <div className="flex gap-2 mb-4">
+                <input 
+                    type="text" 
+                    value={newTask} 
+                    onChange={(e) => SetNewTask(e.target.value)}
+                    className="border p-2 rounded w-full"
+                    placeholder="Enter a new task..."
+                    />
+                    <button
+                        onClick={addTask}
+                        className="bg-blue-500 text-white px-4 py-2 rounded"
+                        >
+                        Add
+                    </button>
+            </div>
+
+            <TaskList tasks={tasks} onToggleTask={toggleTask} onDeleteTask={deleteTask}/>
         </div>
     );
 }
